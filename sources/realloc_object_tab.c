714d3d848500b131ecbaa233379ce63f99e553ff
jerem <jeremy.elalouf@epitech.eu>
2022-04-27 09:27:04 +0200

    fix: initialize variable, add const qualifier for some variables (pr)

--- sources/realloc_object_tab.c
+++ sources/realloc_object_tab.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include "raytracer.h"
 
-static int get_object_tab_len(struct object **tab)
+static int get_object_tab_len(const struct object **tab)
 {
     int i = 0;
 
@@ -17,9 +17,9 @@ static int get_object_tab_len(struct object **tab)
     return i;
 }
 
-struct object **realloc_object_tab(struct object **tab, int new_size)
+struct object **realloc_object_tab(struct object **tab, int const new_size)
 {
-    int tab_len;
+    int tab_len = 0;
     struct object **new = NULL;
 
     if (tab == NULL) {
@@ -29,7 +29,7 @@ struct object **realloc_object_tab(struct object **tab, int new_size)
         tab[new_size - 1] = NULL;
         return tab;
     }
-    tab_len = get_object_tab_len(tab);
+    tab_len = get_object_tab_len((const struct object **)tab);
     if (tab_len == new_size)
         return tab;
     new = malloc(sizeof(struct object *) * (new_size));
