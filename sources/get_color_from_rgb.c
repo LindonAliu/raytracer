bad1f2e20606fc0138d5f13710210e993abf19ec
jerem <jeremy.elalouf@epitech.eu>
2022-05-01 17:09:17 +0200

    feat: set rgba -> a = 255

--- sources/get_color_from_rgb.c
+++ sources/get_color_from_rgb.c
@@ -34,6 +34,6 @@ sfColor get_color_from_rgb(const char *rgb_code)
     result.r = hexa_to_decimal(rgb_code);
     result.g = hexa_to_decimal(rgb_code + 2);
     result.b = hexa_to_decimal(rgb_code + 4);
-    result.a = 0;
+    result.a = 255;
     return result;
 }
